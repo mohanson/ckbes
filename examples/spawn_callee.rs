f487396c437255ff4794d1457a449c3eeffd618b
Mohanson <mohanson@outlook.com>
2024-09-23 21:09:54 +0800

    Implement syscall ckb2023

---
+++ examples/spawn_callee.rs
@@ -0,0 +1,13 @@
+#![no_main]
+#![no_std]
+
+extern crate ckbes;
+
+#[no_mangle]
+pub unsafe extern "C" fn main() -> u64 {
+    let fds = ckbes::syscall::inherited_fds();
+    let out = ckbes::ARGS.join("");
+    ckbes::syscall::write(fds[0], out.as_bytes());
+    ckbes::syscall::close(fds[0]);
+    return 0;
+}
---
+++ examples/spawn_caller.rs
@@ -0,0 +1,18 @@
+#![no_main]
+#![no_std]
+
+extern crate alloc;
+extern crate ckbes;
+
+#[no_mangle]
+pub unsafe extern "C" fn main() -> u64 {
+    let argv = ["Hello", " ", "World", "!"];
+    let fds = ckbes::syscall::pipe();
+    let pid = ckbes::syscall::spawn(1, ckbes::conversion::SOURCE_CELL_DEP, &argv, &[fds[1]]);
+    let mut buf: [u8; 256] = [0; 256];
+    let len = ckbes::syscall::read(fds[0], &mut buf);
+    assert_eq!(len, 12);
+    assert_eq!(&buf[..len as usize], b"Hello World!");
+    ckbes::syscall::wait(pid);
+    return 0;
+}
