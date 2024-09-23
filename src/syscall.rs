51d9178bad0070fa48c5be2c6ae93c18cd09534c
Mohanson <mohanson@outlook.com>
2024-09-23 19:09:19 +0800

    Implement syscall load_block_extension

--- src/syscall.rs
+++ src/syscall.rs
@@ -1,3 +1,4 @@
+use alloc::ffi::CString;
 use alloc::vec::Vec;
 
 pub fn current_cycles() -> u64 {
@@ -27,15 +28,26 @@ pub fn ecall(mut a0: u64, a1: u64, a2: u64, a3: u64, a4: u64, a5: u64, a6: u64, 
     a0
 }
 
+pub fn exec(index: u64, source: u64, args: &[&str]) -> ! {
+    let args_vec: Vec<u64> = args.iter().map(|e| CString::new(*e).unwrap().as_c_str().as_ptr() as u64).collect();
+    let args_ptr = args_vec.as_ptr() as u64;
+    let ret = ecall(index, source, 0, 0, args.len() as u64, args_ptr, 0, 2043);
+    assert!(ret == 0);
+    loop {}
+}
+
 pub fn exit(code: u64) -> ! {
     ecall(code, 0, 0, 0, 0, 0, 0, 93);
     loop {}
 }
 
-pub fn exec(index: u64) -> ! {
-    let ret = ecall(index, crate::conversion::SOURCE_CELL_DEP, 0, 0, 0, 0, 0, 2043);
+pub fn load_block_extension(index: u64, source: u64) -> Vec<u8> {
+    let mut buf = [0; 32 * 1024];
+    let mut len: u64 = 32 * 1024;
+    let ret = ecall(buf.as_mut_ptr() as u64, core::ptr::addr_of_mut!(len) as u64, 0, index, source, 0, 0, 2104);
     assert!(ret == 0);
-    loop {}
+    assert!(len <= 32 * 1024);
+    buf[..len as usize].to_vec()
 }
 
 pub fn load_cell(index: u64, source: u64) -> crate::conversion::CellOutput {
