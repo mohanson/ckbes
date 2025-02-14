e88ea16c3b055ae66ea62c78f3e2801ffcb283b5
mohanson <mohanson@outlook.com>
2025-02-14 11:21:06 +0800

    Fix mutable reference to mutable static

--- src/global.rs
+++ src/global.rs
@@ -1,10 +1,10 @@
 use alloc::string::{String, ToString};
 use alloc::vec::Vec;
 
-pub static mut HEAP: [u8; 1024 * 1024] = [0; 1024 * 1024];
+pub static HEAP: spinning_top::Spinlock<[u8; 1024 * 1024]> = spinning_top::Spinlock::new([0; 1024 * 1024]);
 #[global_allocator]
-pub static mut LALC: linked_list_allocator::LockedHeap = linked_list_allocator::LockedHeap::empty();
-pub static mut ARGS: Vec<String> = Vec::new();
+pub static LALC: linked_list_allocator::LockedHeap = linked_list_allocator::LockedHeap::empty();
+pub static ARGS: spinning_top::Spinlock<Vec<String>> = spinning_top::Spinlock::new(Vec::new());
 
 #[panic_handler]
 pub fn panic_handler(i: &core::panic::PanicInfo) -> ! {
@@ -29,12 +29,12 @@ pub unsafe extern "C" fn _start() {
 #[no_mangle]
 pub unsafe extern "C" fn _entry(argc: u64, argv: *const *const i8) {
     unsafe {
-        LALC.lock().init(HEAP.as_mut_ptr(), 1024 * 1024);
+        LALC.lock().init(HEAP.lock().as_mut_ptr(), 1024 * 1024);
     }
     for i in 0..argc {
         let argn = core::ffi::CStr::from_ptr(argv.add(i as usize).read());
         let argn = String::from(argn.to_string_lossy());
-        ARGS.push(argn);
+        ARGS.lock().push(argn);
     }
     core::arch::asm!("call main");
 }
