b2af62e0feb813ea7641d04dfef59fe1c2028d71
mohanson <mohanson@outlook.com>
2025-02-26 14:09:35 +0800

    Add comments for blake2b

--- src/blake2b.rs
+++ src/blake2b.rs
@@ -1,3 +1,4 @@
+/// Generates a 256-bit (32-byte) blake2b hash from the input data.
 pub fn blake2b_256<T: AsRef<[u8]>>(data: T) -> [u8; 32] {
     let mut p = blake2ya::blake2b_params();
     p.digest(32);
@@ -9,6 +10,7 @@ pub fn blake2b_256<T: AsRef<[u8]>>(data: T) -> [u8; 32] {
     r
 }
 
+/// Generates a 160-bit (20-byte) blake2b hash by truncating the output of blake2b_256.
 pub fn blake2b_160<T: AsRef<[u8]>>(data: T) -> [u8; 20] {
     let h = blake2b_256(data);
     let mut r = [0; 20];
