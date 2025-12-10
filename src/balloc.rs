efdeb4a45cc196972d5af8010659ddeebc7043fe
Mohanson <mohanson@outlook.com>
2025-12-10 15:38:32 +0800

    Refactor buddy allocator

--- src/balloc.rs
+++ src/balloc.rs
@@ -7,26 +7,33 @@
 
 use core::alloc::{GlobalAlloc, Layout};
 use core::cell::UnsafeCell;
+use core::cmp::min;
 use core::ptr::{self, addr_of_mut};
 
-pub static MIN_BLOCK: usize = 64;
-pub static MAX_ORDER: usize = 14;
-pub static MAX_TOTAL: usize = 1024 * 1024;
+pub const MIN_BLOCK: usize = 64;
+pub const MAX_ORDER: usize = 14;
+pub const MAX_TOTAL: usize = 1024 * 1024;
+pub const PTR_ALLOC: *mut u8 = addr_of_mut!(PRE_ALLOC) as *mut u8;
 pub static mut FREE_LIST: [usize; MAX_ORDER + 1] = {
     let mut list = [usize::MAX; MAX_ORDER + 1];
     list[MAX_ORDER] = 0;
     list
 };
 pub static mut PRE_ALLOC: [u8; MAX_TOTAL] = {
+    assert!(matches!(usize::BITS, 32 | 64));
     let mut alloc = [0; MAX_TOTAL];
-    alloc[0] = 0xff;
-    alloc[1] = 0xff;
-    alloc[2] = 0xff;
-    alloc[3] = 0xff;
-    alloc[4] = 0xff;
-    alloc[5] = 0xff;
-    alloc[6] = 0xff;
-    alloc[7] = 0xff;
+    if usize::BITS >= 32 {
+        alloc[0] = 0xff;
+        alloc[1] = 0xff;
+        alloc[2] = 0xff;
+        alloc[3] = 0xff;
+    }
+    if usize::BITS >= 64 {
+        alloc[4] = 0xff;
+        alloc[5] = 0xff;
+        alloc[6] = 0xff;
+        alloc[7] = 0xff;
+    }
     alloc
 };
 
@@ -35,14 +42,14 @@ pub struct Algorithm;
 impl Algorithm {
     pub fn alloc_block(&mut self, order: usize) -> *mut u8 {
         unsafe {
+            if order > MAX_ORDER {
+                return ptr::null_mut();
+            }
             if FREE_LIST[order] != usize::MAX {
                 let block_offset = FREE_LIST[order];
-                let block_ptr = (addr_of_mut!(PRE_ALLOC) as *mut u8).add(block_offset);
-                FREE_LIST[order] = *(block_ptr as *const usize);
+                let block_ptr = PTR_ALLOC.add(block_offset);
+                FREE_LIST[order] = uldr(block_ptr);
                 return block_ptr;
-            }
-            if order >= MAX_ORDER {
-                return ptr::null_mut();
             }
             let block = self.alloc_block(order + 1);
             if block.is_null() {
@@ -50,8 +57,8 @@ impl Algorithm {
             }
             let block_size = MIN_BLOCK << order;
             let buddy_ptr = block.add(block_size);
-            let buddy_offset = buddy_ptr.offset_from(addr_of_mut!(PRE_ALLOC) as *const u8) as usize;
-            *(buddy_ptr as *mut usize) = usize::MAX;
+            let buddy_offset = buddy_ptr.offset_from(PTR_ALLOC) as usize;
+            ustr(buddy_ptr, usize::MAX);
             FREE_LIST[order] = buddy_offset;
             block
         }
@@ -62,20 +69,19 @@ impl Algorithm {
             if ptr.is_null() {
                 return;
             }
-            let start_ptr = addr_of_mut!(PRE_ALLOC) as *const u8;
             let block_ptr = ptr;
             let block_size = MIN_BLOCK << order;
-            let block_offset = block_ptr.offset_from(start_ptr) as usize;
+            let block_offset = block_ptr.offset_from(PTR_ALLOC) as usize;
             let block_idx = block_offset / block_size;
             let buddy_idx = block_idx ^ 1;
             let buddy_offset = buddy_idx * block_size;
-            let buddy_ptr = start_ptr.add(buddy_offset) as *mut u8;
+            let buddy_ptr = PTR_ALLOC.add(buddy_offset);
             if self.buddy_unused(order, buddy_ptr) {
                 self.buddy_close(order, buddy_ptr);
-                self.close_block(order + 1, core::cmp::min(block_ptr, buddy_ptr));
+                self.close_block(order + 1, min(block_ptr, buddy_ptr));
                 return;
             }
-            *(block_ptr as *mut usize) = FREE_LIST[order];
+            ustr(block_ptr, FREE_LIST[order]);
             FREE_LIST[order] = block_offset;
         }
     }
@@ -90,41 +96,39 @@ impl Algorithm {
 
     fn buddy_unused(&mut self, order: usize, ptr: *mut u8) -> bool {
         unsafe {
-            let base = addr_of_mut!(PRE_ALLOC) as *const u8;
-            let need = ptr.offset_from(addr_of_mut!(PRE_ALLOC) as *const u8) as usize;
-            let mut prev = FREE_LIST[order];
+            let buddy_offset = ptr.offset_from(PTR_ALLOC) as usize;
+            let mut block_offset = FREE_LIST[order];
             loop {
-                if prev == need {
-                    break prev != usize::MAX;
+                if block_offset == buddy_offset {
+                    break block_offset != usize::MAX;
                 }
-                if prev == usize::MAX {
-                    break prev == need;
+                if block_offset == usize::MAX {
+                    break block_offset == buddy_offset;
                 }
-                let next = *(base.add(prev) as *const usize);
-                prev = next;
+                block_offset = uldr(PTR_ALLOC.add(block_offset));
             }
         }
     }
 
     fn buddy_close(&mut self, order: usize, ptr: *mut u8) {
         unsafe {
-            let base = addr_of_mut!(PRE_ALLOC) as *const u8;
-            let need = ptr.offset_from(addr_of_mut!(PRE_ALLOC) as *const u8) as usize;
-            let mut prev = FREE_LIST[order];
-            if prev == need {
-                FREE_LIST[order] = *(base.add(need) as *const usize);
+            let buddy_offset = ptr.offset_from(PTR_ALLOC) as usize;
+            let mut block_offset = FREE_LIST[order];
+            let mut block_offtmp: usize;
+            if block_offset == buddy_offset {
+                FREE_LIST[order] = uldr(PTR_ALLOC.add(block_offset));
                 return;
             }
             loop {
-                if prev == usize::MAX {
+                if block_offset == usize::MAX {
                     break;
                 }
-                let next = *(base.add(prev) as *const usize);
-                if next == need {
-                    *(base.add(prev) as *mut usize) = *(base.add(next) as *const usize);
+                block_offtmp = uldr(PTR_ALLOC.add(block_offset));
+                if block_offtmp == buddy_offset {
+                    ustr(PTR_ALLOC.add(block_offset), uldr(PTR_ALLOC.add(block_offtmp)));
                     break;
                 }
-                prev = next;
+                block_offset = block_offtmp;
             }
         }
     }
@@ -165,4 +169,14 @@ fn log2(m: usize, n: usize) -> usize {
         m <<= 1;
     }
     unreachable!()
+}
+
+fn uldr(p: *mut u8) -> usize {
+    unsafe { *(p as *const usize) }
+}
+
+fn ustr(p: *mut u8, n: usize) {
+    unsafe {
+        *(p as *mut usize) = n;
+    }
 }
